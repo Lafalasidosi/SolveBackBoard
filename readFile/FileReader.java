e93d5fac3ee1799432e19387d9b208b6d1369f08
Lafalasidosi <lafalasidosi@pm.me>
2022-03-03 14:12:41 -0800

    create Casperson input translation

---
+++ readFile/FileReader.java
@@ -0,0 +1,33 @@
+package readFile;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FileReader{
+
+    public static ArrayList<String[]> getBoards(String[] args){
+        ArrayList<String[]> results = new ArrayList<String[]>(0);
+
+        try(Scanner input = new Scanner(Paths.get("data.txt"))){
+            while(input.hasNext()){
+                results.add(input.nextLine().replaceAll("[\\s+]", "").replace(';', ',').split(","));
+            }
+        } catch(IOException e){
+            System.out.println("Done goofed.");
+            System.exit(1);
+        }
+
+        return results;
+    }
+
+    public static int[] extractBoard(String[] input){
+        
+        int[] result = new int[24];
+        for(int i = 2; i < 26; i++)
+            result[i-2] = Integer.parseInt(input[i]);
+       
+        return result;
+    }
+}
